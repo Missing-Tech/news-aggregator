abf6f02af73e0ffe03f713550cc7cbb944a6dae1
Missing-Tech <josephgrabski31@gmail.com>
2023-07-20 18:05:06 +0100

    Add sign out

--- src/routes/headlines/+layout.svelte
+++ src/routes/headlines/+layout.svelte
@@ -1,4 +1,14 @@
 <script lang="ts">
+  import { goto } from "$app/navigation";
+  import { auth } from "$lib/firebase";
+  import { signOut } from "firebase/auth";
+
+  async function logOut() {
+    const res = await fetch("/headlines", {
+      method: "DELETE",
+    });
+    await signOut(auth).then(() => goto("/"));
+  }
 </script>
 
 <div class="navbar bg-base-100">
@@ -19,40 +29,11 @@
           /></svg
         >
       </label>
-      <ul
-        tabindex="0"
-        class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-      >
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul class="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
-      </ul>
     </div>
-    <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
-  </div>
-  <div class="navbar-center hidden lg:flex">
-    <ul class="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li tabindex="0">
-        <details>
-          <summary>Parent</summary>
-          <ul class="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
-    </ul>
+    <a class="btn btn-ghost normal-case text-xl" href="/headlines">News</a>
   </div>
   <div class="navbar-end">
-    <a class="btn">Button</a>
+    <a class="btn" on:click={logOut}>Sign Out</a>
   </div>
 </div>
 
