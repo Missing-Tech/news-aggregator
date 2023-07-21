fcc4736de5405d18a0856b6eab5ad6591091aee6
Missing-Tech <josephgrabski31@gmail.com>
2023-07-21 17:30:09 +0100

    Add very basic account settings page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,9 +5,7 @@
 </script>
 
 <div class="flex flex-col items-center pt-72 gap-5">
-  <FirebaseApp {firestore} {auth}>
-    <h1 class="text-5xl uppercase font-serif">News Aggregator</h1>
-    <a class=" btn btn-primary" href="/login">Log in</a>
-    <a class=" btn btn-ghost" href="/signup">No account? Sign up</a>
-  </FirebaseApp>
+  <h1 class="text-5xl uppercase font-serif">News Aggregator</h1>
+  <a class=" btn btn-primary" href="/login">Log in</a>
+  <a class=" btn btn-ghost" href="/signup">No account? Sign up</a>
 </div>
---
+++ src/routes/account/+page.svelte
@@ -0,0 +1,31 @@
+<script>
+  import { currentUser } from "$lib/stores";
+</script>
+
+<div class="flex flex-col justify-center items-center gap-10 h-screen">
+  <h1 class="text-3xl font-serif uppercase">Account Details</h1>
+  <form class=" flex flex-col items-center justify-center gap-5" method="post">
+    <input
+      type="text"
+      name="email"
+      placeholder="Email"
+      required
+      value={$currentUser?.email}
+      class="input input-bordered w-full max-w-xs"
+    />
+    <input
+      type="text"
+      name="username"
+      placeholder="Add a username"
+      value={$currentUser?.email}
+      class="input input-bordered w-full max-w-xs"
+    />
+    <input
+      type="submit"
+      class="btn btn-secondary w-full max-w-xs"
+      value="Update"
+    />
+
+    <a href="/login" class="btn btn-ghost w-full max-w-xs">Forgot password?</a>
+  </form>
+</div>
