164ba5a67447195ba2a7646b5180d4e5fe282f41
Missing-Tech <josephgrabski31@gmail.com>
2023-07-22 11:49:42 +0100

    Prototype grid select

---
+++ src/lib/components/GridSelectItem.svelte
@@ -0,0 +1,15 @@
+<script>
+  let selected = false;
+</script>
+
+<div
+  class="w-32 bg-slate-200 aspect-square rounded-lg hover:bg-slate-300 cursor-pointer indicator"
+  on:click={() => (selected = !selected)}
+>
+  {#if selected}
+    <span class=" indicator-item absolute badge badge-primary"
+      ><i class="fa-solid fa-check" /></span
+    >
+  {/if}
+  <h2>NYT</h2>
+</div>
--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -1,11 +1,19 @@
 <script lang="ts">
-  import ErrorBanner from "./../../lib/components/ErrorBanner.svelte";
+  import GridSelectItem from "$lib/components/GridSelectItem.svelte";
+  import ErrorBanner from "$lib/components/ErrorBanner.svelte";
   import { enhance } from "$app/forms";
-  import { page } from "$app/stores";
-  import { fade } from "svelte/transition";
+  import { fade, fly } from "svelte/transition";
   export let form;
 
   let loading = false;
+  let step = 1;
+
+  function incrementStep() {
+    if (step < 3) step++;
+  }
+  function decrementStep() {
+    if (step > 0) step--;
+  }
 </script>
 
 <div class="flex flex-col justify-center items-center gap-10 h-screen">
@@ -16,6 +24,36 @@
   {#if loading}
     <span in:fade class="loading loading-dots loading-lg" />
   {/if}
+  <ul class="steps">
+    <li
+      class="step"
+      class:step-primary={step >= 0}
+      class:step-secondary={step == 3}
+    >
+      Register
+    </li>
+    <li
+      class="step"
+      class:step-primary={step >= 1}
+      class:step-secondary={step == 3}
+    >
+      Choose Outlets
+    </li>
+    <li
+      class="step"
+      class:step-primary={step >= 2}
+      class:step-secondary={step == 3}
+    >
+      Edit Profile
+    </li>
+    <li
+      class="step"
+      class:step-primary={step >= 3}
+      class:step-secondary={step == 3}
+    >
+      Start reading
+    </li>
+  </ul>
   <form
     class=" flex flex-col items-center justify-center gap-5"
     method="post"
@@ -28,21 +66,45 @@
       };
     }}
   >
+    {#if step == 0}
+      <input
+        type="text"
+        name="email"
+        placeholder="Email"
+        required
+        in:fly={{ x: -20 }}
+        class="input input-bordered w-full max-w-xs"
+      />
+      <input
+        type="password"
+        name="password"
+        placeholder="Password"
+        required
+        in:fly={{ x: -20 }}
+        class="input input-bordered w-full max-w-xs"
+      />
+    {:else if step == 1}
+      <div class="grid grid-cols-3 gap-3" in:fly={{ x: -20 }}>
+        {#each { length: 9 } as _, i}
+          <GridSelectItem />
+        {/each}
+      </div>
+    {:else if step == 3}
+      <input type="submit" class="btn btn-secondary w-full max-w-xs" />
+    {/if}
+    {#if step != 3}
+      <input
+        class="btn btn-primary w-full max-w-xs"
+        value="Next"
+        in:fly={{ x: -20 }}
+        on:click={incrementStep}
+      />
+    {/if}
     <input
-      type="text"
-      name="email"
-      placeholder="Email"
-      required
-      class="input input-bordered w-full max-w-xs"
+      class="btn btn-ghost w-full max-w-xs"
+      value="Back"
+      on:click={decrementStep}
     />
-    <input
-      type="password"
-      name="password"
-      placeholder="Password"
-      required
-      class="input input-bordered w-full max-w-xs"
-    />
-    <input type="submit" class="btn btn-secondary w-full max-w-xs" />
 
     <a href="/login" class="btn btn-ghost w-full max-w-xs">Log in instead</a>
   </form>
